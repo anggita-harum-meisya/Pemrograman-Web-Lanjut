2bc548c5a005f4996487a43ec8cc1b1372e82099
anggita-harum-meisya <1112416089@mhs.dinus.ac.id>
2026-05-26 12:34:25 +0700

    update validation dan login

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -3,45 +3,78 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use CodeIgniter\HTTP\RequestInterface;
+use App\Models\UserModel;
 
 class AuthController extends BaseController
 {
-    function __construct()
+    protected $userModel;
+
+    public function __construct()
     {
         helper('form');
+        $this->userModel = new UserModel();
     }
+
+
 
     public function login()
     {
-        if ($this->request->getPost()) {
+    if ($this->request->getPost()) {
+        $rules = [
+            'username' => 'required|min_length[6]',
+            'password' => 'required|min_length[7]|numeric',
+        ];
+            
+        if ($this->validate($rules)) {
             $username = $this->request->getVar('username');
             $password = $this->request->getVar('password');
 
-            $dataUser = ['username' => 'april', 'password' => '202cb962ac59075b964b07152d234b70', 'role' => 'admin']; // passw 123
+            $dataUser = $this->userModel
+                ->where('username', $username)
+                ->first();
 
-            if ($username == $dataUser['username']) {
-                if (md5($password) == $dataUser['password']) {
+            if ($dataUser) {
+
+                if (password_verify($password, $dataUser['password'])) {
+
                     session()->set([
-                        'username' => $dataUser['username'],
-                        'role' => $dataUser['role'],
-                        'isLoggedIn' => TRUE,
-                        'email' => 'admin@gmail.com',
-                        'time' => date('Y-m-d H:i:s')
+                        'username'   => $dataUser['username'],
+                        'role'       => $dataUser['role'],
+                        'isLoggedIn' => true,
+                        'email'      => 'admin@gmail.com',
+                        'time'       => date('Y-m-d H:i:s')
                     ]);
 
                     return redirect()->to(base_url('/'));
+
+                
                 } else {
-                    session()->setFlashdata('failed', 'Username & Password Salah');
+                    session()->setFlashdata('failed', $this->validator->listErrors());
                     return redirect()->back();
                 }
+                    } else {
+
+                    session()->setFlashdata(
+                        'failed',
+                        'Password Salah'
+                    );
+
+                    return redirect()->back();
+                }
+
             } else {
-                session()->setFlashdata('failed', 'Username Tidak Ditemukan');
+
+                session()->setFlashdata(
+                    'failed',
+                    'Username Tidak Ditemukan'
+                );
+
                 return redirect()->back();
             }
-        } else {
-            return view('v_login');
+
         }
+
+        return view('v_login');
     }
 
     public function logout()
