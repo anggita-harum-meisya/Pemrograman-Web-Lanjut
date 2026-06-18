116f60580ac3d6f92881f1912c73c6c921004b58
anggita-harum-meisya <1112416089@mhs.dinus.ac.id>
2026-06-18 15:33:26 +0700

    capstone 10 webservice client

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -26,7 +26,11 @@ $routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
     $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
     $routes->get('clear', 'TransaksiController::cart_clear');
 });
-$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
+
+$routes->get('checkout', 'TransaksiController::checkout', ['filter' => 'auth']);
+$routes->post('buy', 'TransaksiController::buy', ['filter' => 'auth']);
+$routes->get('ajax/destinations','TransaksiController::destinations', ['filter' => 'auth']);
+$routes->get('ajax/costs','TransaksiController::costs', ['filter' => 'auth']);
 $routes->get('faq', 'Home::faq', ['filter' => 'auth']);
 $routes->get('profile', 'Home::profile', ['filter' => 'auth']);
 $routes->get('contact', 'Home::contact', ['filter' => 'auth']);
